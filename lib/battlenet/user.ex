9858dc79d6e464330712400a4b87147f11e6936b
Daniel Grieve <git@cazrin.net>
2015-12-31 21:24:56 +0000

    Retrieve user with their access token

--- lib/battlenet/user.ex
+++ lib/battlenet/user.ex
@@ -1,3 +1,21 @@
 defmodule Battlenet.User do
   defstruct id: nil, battletag: nil
+
+  def with_token(access_token) do
+    case HTTPoison.get(resource_url("account/user", access_token)) do
+      {:ok, %HTTPoison.Response{body: body}} ->
+        body
+        |> Poison.decode!(as: Battlenet.User)
+    end
+  end
+
+  defp api_url, do: config_api_site_url || "https://#{region}.api.battle.net"
+  defp resource_url(path, access_token) do
+    "#{api_url}/#{path}?access_token=#{access_token}"
+  end
+
+  defp config_api_site_url, do: Application.get_env(:battlenet, :api_site_url)
+  defp region do
+    Application.get_env(:battlenet, :region) || "eu"
+  end
 end
--- test/user_test.exs
+++ test/user_test.exs
@@ -6,4 +6,21 @@ defmodule Battlenet.UserTest do
     assert user.id == nil
     assert user.battletag == nil
   end
+
+  test "retrieving user with access token" do
+    bypass = Bypass.open
+
+    Bypass.expect bypass, fn conn ->
+      assert conn.request_path == "/account/user"
+      assert conn.method == "GET"
+
+      Plug.Conn.resp(conn, 200, "{\"id\": \"user-id\", \"battletag\": \"User#1234\"}")
+    end
+
+    expected_user = %Battlenet.User{id: "user-id", battletag: "User#1234"}
+
+    Application.put_env(:battlenet, :api_site_url, "http://localhost:#{bypass.port}")
+    Battlenet.User.with_token("a-token") == expected_user
+    Application.delete_env(:battlenet, :api_site_url)
+  end
 end
