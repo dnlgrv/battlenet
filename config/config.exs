441c81337efdf997d548a5379639e0f1d2f219f5
Daniel Grieve <git@cazrin.net>
2015-12-31 20:30:50 +0000

    Cover basic auth usage in tests

--- config/config.exs
+++ config/config.exs
@@ -2,5 +2,6 @@ use Mix.Config
 
 config :battlenet,
   client_id: "test-client-id",
+  client_secret: "test-client-secret",
   redirect_uri: "https://localhost.test/auth/callback",
   region: "test"
--- test/auth_test.exs
+++ test/auth_test.exs
@@ -31,12 +31,27 @@ defmodule Battlenet.AuthTest do
       assert body["grant_type"] == "authorization_code"
       assert body["redirect_uri"] == @config[:redirect_uri]
 
+      [client_id, client_secret] = basic_auth_credentials(conn)
+      assert client_id == @config[:client_id]
+      assert client_secret == @config[:client_secret]
+
       Plug.Conn.resp(conn, 200, "{\"access_token\": \"test-token\"}")
     end
 
     Application.put_env(:battlenet, :site_url, "http://localhost:#{bypass.port}")
     assert Battlenet.Auth.token("test-auth-code") == "test-token"
     Application.delete_env(:battlenet, :site_url)
+  end
+
+  defp basic_auth_credentials(conn) do
+    {_header, authorization_body} = Enum.find(conn.req_headers, fn({header, _body}) ->
+      header == "authorization"
+    end)
+    ["Basic", base64_encoded_body] = String.split(authorization_body, " ")
+
+    base64_encoded_body
+    |> Base.decode64!()
+    |> String.split(":")
   end
 
   defp query_param(url, param) do
