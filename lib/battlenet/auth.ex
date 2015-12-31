9e68d0dfa749c35de571b39f93a8afa87be9536d
Daniel Grieve <git@cazrin.net>
2015-12-31 18:43:47 +0000

    Retrieve access token from bnet

--- lib/battlenet/auth.ex
+++ lib/battlenet/auth.ex
@@ -1,14 +1,36 @@
 defmodule Battlenet.Auth do
   def authorize_url do
-    "https://#{region}.battle.net/oauth/authorize?#{authorize_params}"
+    "#{site_url}/oauth/authorize?#{authorize_params}"
+  end
+
+  def token(code) do
+    req_body = {:form, token_params(code)}
+    req_options = [hackney: [basic_auth: basic_auth_credentials]]
+
+    case HTTPoison.request(:post, token_url, req_body, [], req_options) do
+      {:ok, %HTTPoison.Response{body: body}} ->
+        body
+        |> Poison.decode!()
+        |> Dict.get("access_token")
+    end
   end
 
   defp authorize_params do
     "client_id=#{client_id}&redirect_uri=#{redirect_uri}&response_type=code"
   end
 
-  defp client_id, do: config[:client_id]
+  defp token_params(code) do
+    [code: code,
+     grant_type: "authorization_code",
+     redirect_uri: Dict.get(config, :redirect_uri)]
+  end
+
   defp config, do: Application.get_all_env(:battlenet)
+
+  defp basic_auth_credentials, do: {config[:client_id], config[:client_secret]}
+  defp client_id, do: config[:client_id]
   defp redirect_uri, do: config[:redirect_uri]
   defp region, do: config[:region] || "eu"
+  defp site_url, do: config[:site_url] || "https://#{region}.battle.net"
+  defp token_url, do: "#{site_url}/oauth/token"
 end
--- test/auth_test.exs
+++ test/auth_test.exs
@@ -18,6 +18,27 @@ defmodule Battlenet.AuthTest do
     assert query_param(url, "response_type") == "code"
   end
 
+  test "retrieving access token" do
+    bypass = Bypass.open
+
+    Bypass.expect bypass, fn conn ->
+      assert conn.request_path == "/oauth/token"
+      assert conn.method == "POST"
+
+      {:ok, body_params, _} = Plug.Conn.read_body(conn)
+      body = URI.decode_query(body_params)
+      assert body["code"] == "test-auth-code"
+      assert body["grant_type"] == "authorization_code"
+      assert body["redirect_uri"] == @config[:redirect_uri]
+
+      Plug.Conn.resp(conn, 200, "{\"access_token\": \"test-token\"}")
+    end
+
+    Application.put_env(:battlenet, :site_url, "http://localhost:#{bypass.port}")
+    assert Battlenet.Auth.token("test-auth-code") == "test-token"
+    Application.delete_env(:battlenet, :site_url)
+  end
+
   defp query_param(url, param) do
     url
     |> URI.parse()
